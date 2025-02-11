43d44ba9426f9e920ec6ea132955dc44fe4b50c9
Vitor Yago <42788551+vitoryago@users.noreply.github.com>
2025-02-11 17:13:01 -0300

    Delete tests/teste.py

--- tests/teste.py
+++
@@ -1,8 +0,0 @@
-def fuzz_sum_number(target):
-    result = 0
-    for i in range(target):
-        if i % 3 == 0 or i % 5 == 0:
-            result += i
-    return result
-
-print(fuzz_sum_number(10))
